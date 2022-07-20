7e5e416718cf39dc2651c141fa778f0e3901b307
user <wlsdldos00@naver.com>
2022-07-20 09:38:49 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# 매개변수로 1차 가공된 데이터 프레임을 받는다.
+def damage_avg(df):
+    df = match_df[['CHAMPIONNAME', 'TOTALDAMAGEDEALTTOCHAMPIONS', 'TOTALDAMAGETAKEN']]
+    df2 = df.groupby(['CHAMPIONNAME']).mean()
+    df3 = df2.rename(columns={'TOTALDAMAGEDEALTTOCHAMPIONS': '평균 가한 데미지', 'TOTALDAMAGETAKEN': '평균 받은 데미지'})
+    df3 = df3.reset_index()
+
+    from matplotlib import font_manager, rc
+    font_path = 'C:/Windows/Fonts/gulim.ttc'
+    font = font_manager.FontProperties(fname=font_path).get_name()
+    rc('font', family=font)
+    fig = px.scatter(df3, x='평균 가한 데미지', y='평균 받은 데미지',
+                     hover_data=['CHAMPIONNAME'],
+                     title='강석진_라인별 15분 골드 그래프')
+    fig.show()
